72a632128129aa1589b0e1a81597750199c369e0
zhy <zhyar121@yahoo.com>
2026-03-21 18:43:17 +0100

    Update .lua

--- .lua
+++ .lua
@@ -1,72 +1,117 @@
-function SendMessage(url, message)
-    local http = game:GetService("HttpService")
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+
+
+
+-local WEBHOOK_URL = "https://discord.com/api/webhooks/1484849714557091861/D8IYek8Ek8KcYHXUNsKcC_mHn9ftfAbp8nt7P7-KITJP7LenUwnX_nbs4yxSysP9t5WP"
+
+local function SendMessage(url, message)
     local headers = {
         ["Content-Type"] = "application/json"
     }
+
     local data = {
-        ["content"] = message
+        ["content"] = tostring(message),
+        ["username"] = Digital,
+        ["avatar_url"] = https://cdn.discordapp.com/avatars/1475893793721942097/9dcb14e34cb82adbed9c86d737eac3e8.png?size=512
     }
-    local body = http:JSONEncode(data)
-    local response = request({
-        Url = url,
-        Method = "POST",
-        Headers = headers,
-        Body = body
-    })
-    print("Sent")
+
+    local body = HttpService:JSONEncode(data)
+
+    pcall(function()
+        request({
+            Url = url,
+            Method = "POST",
+            Headers = headers,
+            Body = body
+        })
+    end)
 end
 
-function SendMessageEMBED(url, embed)
-    local http = game:GetService("HttpService")
+local function sendembed(url, embed)
     local headers = {
         ["Content-Type"] = "application/json"
     }
+
     local data = {
+        ["username"] = WEBHOOK_NAME,
+        ["avatar_url"] = WEBHOOK_PFP,
+
         ["embeds"] = {
             {
                 ["title"] = embed.title,
                 ["description"] = embed.description,
                 ["color"] = embed.color,
                 ["fields"] = embed.fields,
+                ["thumbnail"] = embed.thumbnail,
                 ["footer"] = {
                     ["text"] = embed.footer.text
                 }
             }
         }
     }
-    local body = http:JSONEncode(data)
-    local response = request({
-        Url = url,
-        Method = "POST",
-        Headers = headers,
-        Body = body
-    })
-    print("Sent")
+
+    local body = HttpService:JSONEncode(data)
+
+    pcall(function()
+        request({
+            Url = url,
+            Method = "POST",
+            Headers = headers,
+            Body = body
+        })
+    end)
 end
 
+-- thumbnal
+local function avaterThumb(userId)
+    return "https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds="
+        .. userId ..
+        .. "&size=420x420&format=Png&isCircular=false"
+end
 
---Examples 
+-- log
 
-local url = "https://discord.com/api/webhooks/1484849714557091861/D8IYek8Ek8KcYHXUNsKcC_mHn9ftfAbp8nt7P7-KITJP7LenUwnX_nbs4yxSysP9t5WP"
-SendMessage(url, "Hello")
+local function log(player)
+    local embed = {
+        title = "Player Executed Script",
+        description = "Execution detected",
+        color = 65280,
 
-
-local embed = {
-    ["title"] = "This is an embedded message",
-    ["description"] = "This message has an embed with fields and a footer",
-    ["color"] = 65280,
-    ["fields"] = {
-        {
-            ["name"] = "Field 1",
-            ["value"] = "This is the first field"
+        fields = {
+            {name = "Username", value = player.Name, inline = true},
+            {name = "Display Name", value = player.DisplayName, inline = true},
+            {name = "UserId", value = tostring(player.UserId), inline = true},
+            {
+                name = "PlaceId",
+                value = tostring(game.PlaceId),
+                inline = true
+            },
+            {
+                name = "JobId",
+                value = tostring(game.JobId),
+                inline = true
+            }
         },
-        {
-            ["name"] = "Field 2",
-            ["value"] = "This is the second field"
+
+        thumbnail = {
+            url = avaterThumb(player.UserId)
+        },
+
+        footer = {
+            text = "Webhook Logger"
         }
-    },
-    ["footer"] = {
-        ["text"] = "This is the footer text"
     }
-}
-SendMessageEMBED(url, embed)
+
+    sendembed(WEBHOOK_URL, embed)
+end
+
+-- start
+
+if Players.LocalPlayer then
+    log(Players.LocalPlayer)
+end
+
+Players.PlayerAdded:Connect(function(player)
+    log(player)
+end)
